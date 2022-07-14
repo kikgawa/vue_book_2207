4b46cf1d3753ab9b602243c9cd5527bb1b0344b1
Minori Kikugawa <minocolor@gmail.com>
2022-07-14 17:33:13 +0900

    todo update

--- js/todo.js
+++ js/todo.js
@@ -1,20 +1,30 @@
 const vm = new Vue({
   el: '#app',
-  data: {
-    newTodo: '',
-    todos: [],
-    edit: 0
+  data() {
+    return {
+      newTodo: '',
+      todos: [],
+    }
   },
   methods: {
     addTask() {
-      this.todos.push(this.newTodo)
+      if (this.newTodo === '') return
+      let todo = {
+        item: this.newTodo,
+        isActive: false
+      }
+      this.todos.push(todo)
       this.newTodo = ''
     },
     deleteTask(index) {
       this.todos.splice(index, 1);
     },
     editTask(index) {
-      this.edit = index
+      this.todos[index].isActive = true
+      this.todos[index].item = this.todos[index].item
+    },
+    updateTask(index) {
+      this.todos[index].isActive = false
     }
   }
 })
