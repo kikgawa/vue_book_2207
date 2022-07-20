51ed84dc5ee42cf104704482af4f2e5a6f7c24e6
Minori Kikugawa <minocolor@gmail.com>
2022-07-20 18:56:27 +0900

    7vuex

---
+++ sfc/hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <p class="message">メッセージ{{ meg }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "hello",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.message {
+  color: #429999;
+}
+</style>
